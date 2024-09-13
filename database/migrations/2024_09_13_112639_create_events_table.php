5fc1808e38f888d27a427811810ef13ce3aef5d4
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-09-13 15:29:43 +0200

    first commit

---
+++ database/migrations/2024_09_13_112639_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('img')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('latitude', 11, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->date('date');
+            $table->time('time');
+            $table->boolean('reservation_required');
+            $table->decimal('cost', 5, 2)->nullable();
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
