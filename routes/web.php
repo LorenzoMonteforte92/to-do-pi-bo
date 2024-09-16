23461efe51114383a23fcd9a84b38ea315e0c5ca
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-09-16 18:29:18 +0200

    feat: add new dashboard root

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,15 @@ Route::get('/', function () {
 // questo raggruppa tutte le rotte che devono essere coperte da amministrazione facendo in modo che il nome della rotta inizi per admin.
 // e che il prefisso prima di / sia admin nell'url
 Route::middleware(['auth', 'verified'])
-    ->name('admin.')
-    ->prefix('admin')
-    ->group(function () {
-        Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
-    });
+->name('admin.')
+->prefix('admin')
+->group(function() {
+    // Le varie rotte di amministrazione
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    // Route::resource('posts', PostController::class)->parameters([
+    //     'posts' => 'post:slug'
+    // ]);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
