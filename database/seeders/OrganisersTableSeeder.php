0aa3aa05948cd74e0822c15c002064a9901faf28
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-09-18 11:17:36 +0200

    feat: add event type and organisers table seeders

---
+++ database/seeders/OrganisersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Organiser;
+use Illuminate\Support\Str;
+
+class OrganisersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $organisers = [
+            
+            'locale',
+            'associazione',
+            'organizzatore privato',
+            'collettivo'
+        ];
+
+        foreach($organisers as $singleOrganiser){
+            $newOrganiser = new Organiser();
+            $newOrganiser->name = $singleOrganiser;
+            $newOrganiser->slug = Str::slug($newOrganiser->name, '-');
+            $newOrganiser->save();
+    
+        }
+    }
+}
---
+++ database/seeders/TypesTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $eventTypes = [
+
+            'musica',
+            'food',
+            'teatro',
+            'stand up',
+            'sport',
+            'festival',
+            'cinema',
+            'workshop',
+            'arte',
+            'talk',
+            'seminario',
+            'riunione',
+            'open day',
+            'giochi da tavolo',
+
+        ];
+
+        foreach($eventTypes as $singleEventType){
+            $newEventType = new Type();
+            $newEventType->name = $singleEventType;
+            $newEventType->slug = Str::slug($newEventType->name, '-');
+            $newEventType->save();
+    
+        }
+    }
+}
